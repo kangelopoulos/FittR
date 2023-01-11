6de07eb288b2d1fa667452b61fc54453debac038
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-01-11 15:27:09 -0600

    fixing routing issue

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
+const PORT = 3000;
 /**
  * Proper routing for production/development
  */
-if (process.env.NODE_ENV === "production") {
+ if (process.env.NODE_ENV === "production") {
   app.use("/", express.static(path.join(__dirname, "../build")));
   app.get("/", (req, res) => {
     return res
@@ -17,5 +17,6 @@ if (process.env.NODE_ENV === "production") {
     return res.status(200).redirect("/");
   });
 }
+app.use((req, res) => res.sendStatus(404));
 
 app.listen(PORT, () => console.log(`Server now running on port ${PORT}`));
